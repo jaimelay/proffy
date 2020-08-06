54729486ef50c39e9bf2ca1546d723705115c972
jaimelay <jaimelay1997@gmail.com>
2020-08-06 02:43:27 -0300

    Add frontend connection to the backend #3

--- frontend/web/src/components/TeacherItem/index.tsx
+++ frontend/web/src/components/TeacherItem/index.tsx
@@ -2,34 +2,52 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/16366726?s=460&u=a470a73a5629f08282f479d5f8b319d432b692be&v=4" alt="Jaime Lay"/>
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Jaime Lay</strong>
-                    <span>Educação Física</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Entusiasta por Calistenia, Powerlifting e Musculação.
-                <br /> <br />
-                Experiência principal em calistenia.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/Hora
-                    <strong>R$150,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Entrar em contato"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
